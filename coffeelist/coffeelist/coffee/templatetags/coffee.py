8f2f7ad07bce48d4b5b1d584c4f1f2e8ca2ba919
Phillip Berndt <phillip.berndt@googlemail.com>
2015-11-18 11:31:38 +0100

    Use given names to address users in mails

--- coffeelist/coffeelist/coffee/templatetags/coffee.py
+++ coffeelist/coffeelist/coffee/templatetags/coffee.py
@@ -1,6 +1,8 @@
-from django import template
+import re
 
+from django import template
 from django.utils.safestring import mark_safe
+from django.utils.encoding import force_text
 
 register = template.Library()
 
@@ -17,3 +19,23 @@ def amount(value, smiley=False):
         elif value >= 20:
             classes.append("great")
     return mark_safe('<span class="%s">%+02.2f &euro;</span>' % (" ".join(classes), value))
+
+NAME_PATTERNS = map(re.compile, (
+    "(?P<given>[^\s,]+)\s+(?P<family>.+)",
+    "(?P<family>[^\s,]+),\s+(?P<given>.+)",
+))
+
+def parse_name(name):
+    for pattern in NAME_PATTERNS:
+        match = pattern.match(name)
+        if match:
+            return match.groupdict()
+    return False
+
+@register.filter
+def first_name(value):
+    parsed_name = parse_name(force_text(value))
+    if parsed_name:
+        return parsed_name["given"]
+    else:
+        return parsed_name
